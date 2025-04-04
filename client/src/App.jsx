a28d031605dbf8b1bba59d032b104d5488145c39
Jatin khetan <143867227+JK-asthetic@users.noreply.github.com>
2025-04-04 07:51:31 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,16 @@ import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Register from "@/pages/Register";
+import AdminDashboard from "@/pages/Admin";
+import SignupPage from "@/pages/Signup";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/register" component={Register} />
+      <Route path="/admin" component={AdminDashboard} />
+      <Route path="/signup" component={SignupPage} />
       <Route component={NotFound} />
     </Switch>
   );
