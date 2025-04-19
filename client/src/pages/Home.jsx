0300c78ff8465cedff9406f1b6924acf73e0d5d5
Jatin khetan <143867227+JK-asthetic@users.noreply.github.com>
2025-04-19 12:00:53 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,12 +4,11 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Themes from "@/components/Themes";
 import Timeline from "@/components/Timeline";
-// import Legacy from "@/components/Legacy";
 import Faq from "@/components/Faq";
 import Registration from "@/components/Registration";
 import Organizers from "@/components/Organizers";
 import Footer from "@/components/Footer";
-import RoundDetails from "../components/Round_detail";
+import RoundDetails from "@/components/Round_detail";
 
 export default function Home() {
   return (
@@ -20,7 +19,6 @@ export default function Home() {
       <RoundDetails />
       <Themes />
       <Timeline />
-      {/* <Legacy /> */}
       <Faq />
       <Registration />
       <Organizers />
